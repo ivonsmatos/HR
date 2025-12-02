eeafb60f79292541013904c83d956d0d542a1c86
Ivon Matos <ivon.asmatos@senacsp.edu.br>
2025-12-02 16:08:26 -0300

    fix: adicionar SessionMiddleware ao MIDDLEWARE

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -73,6 +73,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
